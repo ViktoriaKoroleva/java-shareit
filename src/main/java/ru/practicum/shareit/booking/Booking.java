42010851a6bd28aee9f00378376aa0aeeea9edcf
viktoriakoroleva <koroleva_vs02@mail.ru>
2024-07-29 19:58:06 +0300

    sprint14-fix9

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,6 @@
-package ru.practicum.shareit.booking.model;
+package ru.practicum.shareit.booking;
 
 import lombok.*;
-import ru.practicum.shareit.booking.status.Status;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
--- src/main/java/ru/practicum/shareit/booking/Status.java
+++ src/main/java/ru/practicum/shareit/booking/Status.java
@@ -1,4 +1,4 @@
-package ru.practicum.shareit.booking.status;
+package ru.practicum.shareit.booking;
 
 public enum Status {
     WAITING,
