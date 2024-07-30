4b1a083b73fd8ec29b74732fd2e8283e4a26a6c8
viktoriakoroleva <koroleva_vs02@mail.ru>
2024-07-30 21:41:16 +0300

    sprint14-fix26

--- src/main/java/ru/practicum/shareit/user/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/UserMapper.java
@@ -7,7 +7,7 @@ public class UserMapper {
 
     public static UserDto toUserDto(User user) {
         return new UserDto(
-                user.getId() != null ? String.valueOf(user.getId()) : null,
+                user.getId(),
                 user.getName(),
                 user.getEmail()
         );
@@ -15,7 +15,7 @@ public class UserMapper {
 
     public static User toUser(UserDto userDto) {
         return new User(
-                userDto.getId() != null ? Long.valueOf(userDto.getId()) : null,
+                userDto.getId(),
                 userDto.getName(),
                 userDto.getEmail()
         );
