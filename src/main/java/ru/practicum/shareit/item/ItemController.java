e6e2ea08edf3b4a9cc66523908f91d524313a3b8
viktoriakoroleva <koroleva_vs02@mail.ru>
2024-07-30 21:27:13 +0300

    sprint14-fix24

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -18,36 +18,35 @@ public class ItemController {
     private final ItemServiceDtoImpl itemService;
 
     @PostMapping
-    public ItemDto add(@RequestHeader(USER_HEADER) Long userId,
+    public ItemDto add(@RequestHeader(USER_HEADER) String userId, // Изменено с Long на String
                        @Valid @RequestBody ItemDto itemDto) {
         log.info("POST Запрос на добавление пользователем с id = " + userId + " предмета " + itemDto.toString());
         return itemService.add(userId, itemDto);
     }
 
     @PatchMapping("/{itemId}")
-    public ItemDto update(@RequestHeader(USER_HEADER) Long userId,
+    public ItemDto update(@RequestHeader(USER_HEADER) String userId, // Изменено с Long на String
                           @RequestBody ItemDto itemDto,
-                          @PathVariable Long itemId) {
+                          @PathVariable String itemId) { // Изменено с Long на String
         log.info("PATCH Запрос на обновление предмета с id = " + itemId + " пользователем с id = " + userId);
         return itemService.update(userId, itemId, itemDto);
     }
 
     @GetMapping("/{itemId}")
-    public ItemDto findById(@RequestHeader(USER_HEADER) Long userId,
-                            @PathVariable("itemId")
-                            Long itemId) {
+    public ItemDto findById(@RequestHeader(USER_HEADER) String userId, // Изменено с Long на String
+                            @PathVariable("itemId") String itemId) { // Изменено с Long на String
         log.info("GET Запрос на получение предмета с id = " + itemId + " пользователем с id = " + userId);
         return itemService.findItemById(userId, itemId);
     }
 
     @GetMapping
-    public List<ItemDto> findAll(@RequestHeader(USER_HEADER) Long userId) {
+    public List<ItemDto> findAll(@RequestHeader(USER_HEADER) String userId) { // Изменено с Long на String
         log.info("GET Запрос на получение предметов пользователя с id = " + userId);
         return itemService.findAll(userId);
     }
 
     @GetMapping("/search")
-    public List<ItemDto> searchItems(@RequestHeader(USER_HEADER) Long userId,
+    public List<ItemDto> searchItems(@RequestHeader(USER_HEADER) String userId, // Изменено с Long на String
                                      @RequestParam(name = "text") String text) {
         log.info("GET Запрос на поиск предметов");
         return itemService.search(userId, text);
