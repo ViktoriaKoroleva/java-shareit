d672bb9027661f68898d0ed0e7ec1c6772d1671c
viktoriakoroleva <koroleva_vs02@mail.ru>
2024-08-04 19:27:22 +0300

    sprint14-fix38

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.item.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.HeaderConstants;
 import ru.practicum.shareit.item.dto.CreateItemRequest;
 import ru.practicum.shareit.item.dto.UpdateItemRequest;
 import ru.practicum.shareit.item.model.Item;
@@ -19,12 +20,12 @@ public class ItemController {
 
     @PostMapping
     public Item createItem(@Valid @RequestBody CreateItemRequest itemDto,
-                           @RequestHeader("X-Sharer-User-Id") int userId) {
+                           @RequestHeader(HeaderConstants.USER_ID_HEADER) int userId) {
         return itemService.createItem(userId, itemDto);
     }
 
     @PatchMapping("/{itemId}")
-    public Item updateItem(@RequestHeader("X-Sharer-User-Id") int userId,
+    public Item updateItem(@RequestHeader(HeaderConstants.USER_ID_HEADER) int userId,
                            @PathVariable int itemId,
                            @RequestBody UpdateItemRequest itemDto) {
         return itemService.updateItem(userId, itemId, itemDto);
@@ -36,12 +37,12 @@ public class ItemController {
     }
 
     @GetMapping
-    public List<Item> findUserItemsById(@RequestHeader("X-Sharer-User-Id") int userId) {
+    public List<Item> findUserItemsById(@RequestHeader(HeaderConstants.USER_ID_HEADER) int userId) {
         return itemService.findUserItemsById(userId);
     }
 
     @GetMapping("/search")
-    public List<Item> findItemByText(@RequestHeader("X-Sharer-User-Id") int userId,
+    public List<Item> findItemByText(@RequestHeader(HeaderConstants.USER_ID_HEADER) int userId,
                                      @RequestParam("text") String text) {
         if (text.isBlank()) {
             return Collections.emptyList();
