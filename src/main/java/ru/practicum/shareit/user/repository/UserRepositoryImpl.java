dda10e60137ade16658835e2968dcf9ec1db7327
viktoriakoroleva <koroleva_vs02@mail.ru>
2024-08-05 20:00:24 +0300

    sprint14-fix43

--- src/main/java/ru/practicum/shareit/user/repository/UserRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepositoryImpl.java
@@ -25,7 +25,7 @@ public class UserRepositoryImpl implements UserRepository {
     @Override
     public User create(User user) {
         if (isDuplicateEmail(user.getEmail())) {
-            throw new DuplicateEmailException("Email already exists");
+            throw new DuplicateEmailException("Email alread exists");
         }
 
         user.setId(generateId());
