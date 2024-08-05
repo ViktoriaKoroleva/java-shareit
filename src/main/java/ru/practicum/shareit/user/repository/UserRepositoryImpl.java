f9904a9dd5a38a80090e33d37eb3c51a57ee4deb
viktoriakoroleva <koroleva_vs02@mail.ru>
2024-08-05 20:15:05 +0300

    sprint14-fix49

--- src/main/java/ru/practicum/shareit/user/repository/UserRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepositoryImpl.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.exception.modelException.DuplicateEmailException;
 import ru.practicum.shareit.exception.modelException.NotUniqueEmailException;
+import ru.practicum.shareit.exception.modelException.UserNotFoundException;
 import ru.practicum.shareit.user.model.User;
 
 import java.util.ArrayList;
@@ -57,7 +58,7 @@ public class UserRepositoryImpl implements UserRepository {
     @Override
     public User getById(int id) {
         if (!users.containsKey(id)) {
-            throw new NotUniqueEmailException("No User with such id");
+            throw new UserNotFoundException("No User with such id");
         }
 
         return users.get(id);
