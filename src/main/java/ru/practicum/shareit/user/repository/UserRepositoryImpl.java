de1732f46a4386322a6a91e836687312f040f492
viktoriakoroleva <koroleva_vs02@mail.ru>
2024-08-05 19:50:21 +0300

    sprint14-fix41

--- src/main/java/ru/practicum/shareit/user/repository/UserRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepositoryImpl.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.user.repository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.exception.modelException.DuplicateEmailException;
-import ru.practicum.shareit.exception.modelException.NotUniqueEmailException;
 import ru.practicum.shareit.exception.modelException.UserNotFoundException;
 import ru.practicum.shareit.user.model.User;
 
